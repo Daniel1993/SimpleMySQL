c2bd5fa3dce961c2f786cd5572ad1a93363bf640
Daniel1993 <daniel_me@live.com.pt>
2014-05-27 15:39:09 +0100

    Some refractoring

--- SimpleMySQL/src/simplemysql/query/clause/CreateTableClause.java
+++ SimpleMySQL/src/simplemysql/query/clause/CreateTableClause.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package simplemysql.query.clause;
 
 /**
@@ -11,49 +10,55 @@ package simplemysql.query.clause;
  * @author Daniel
  */
 public class CreateTableClause {
-    private final StringBuilder query = new StringBuilder();
 
-    public CreateTableClause() {
-        query.append("(");
-    }
+  private final StringBuilder query = new StringBuilder();
 
-    /**
-     *
-     * @param column the column name
-     * @param typeAndConstraints column type like INT, VARCHAR(56)... and/or
-     * constraints like NOT NULL, DEFAULT 8...
-     * @return this
-     */
-    public CreateTableClause addColumn(String column, String typeAndConstraints) {
-        query.append("`").append(column).append("` ").append(typeAndConstraints).
-                append(", ");
-        return this;
-    }
+  public CreateTableClause() {
+    query.append("(");
+  }
 
-    public CreateTableClause addPrimaryKey(String column) {
-        query.append("PRIMARY KEY(`").append(column).append("`), ");
-        return this;
-    }
+  /**
+   *
+   * @param column the column name
+   * @param typeAndConstraints column type like INT, VARCHAR(56)... and/or
+   * constraints like NOT NULL, DEFAULT 8...
+   * @return this
+   */
+  public CreateTableClause addColumn(String column, String typeAndConstraints) {
+    query.append("`").append(column).append("` ").append(typeAndConstraints).
+            append(", ");
+    return this;
+  }
 
-    public CreateTableClause addForeignKey(String column, String foreighTable,
-            String foreignColumn, String options) {
-        query.append("FOREIGN KEY(`").append(column).append("`) ").
-                append("REFERENCES `").append(foreighTable).append("`(`").
-                append(foreignColumn).append("`) ").append(options).append(", ");
-        return this;
+  public CreateTableClause addPrimaryKey(String... column) {
+    query.append("PRIMARY KEY(");
+    for (String s : column ) {
+      query.append("`").append(s).append("`, ");
     }
+    query.replace(query.length() - 2, query.length(), "");
+    query.append("), ");
+    return this;
+  }
 
-    public CreateTableClause addForeignKey(String column, String foreighTable,
-            String foreignColumn) {
-        return addForeignKey(column, foreighTable, foreignColumn, "");
-    }
+  public CreateTableClause addForeignKey(String column, String foreighTable,
+          String foreignColumn, String options) {
+    query.append("FOREIGN KEY(`").append(column).append("`) ").
+            append("REFERENCES `").append(foreighTable).append("`(`").
+            append(foreignColumn).append("`) ").append(options).append(", ");
+    return this;
+  }
 
-    @Override
-    public String toString() {
-        if (query.length() > 0) {
-            query.replace(query.length() - 2, query.length(), ") ");
-        }
-        return query.toString();
+  public CreateTableClause addForeignKey(String column, String foreighTable,
+          String foreignColumn) {
+    return addForeignKey(column, foreighTable, foreignColumn, "");
+  }
+
+  @Override
+  public String toString() {
+    if (query.length() > 0) {
+      query.replace(query.length() - 2, query.length(), ") ");
     }
-    
+    return query.toString();
+  }
+
 }
